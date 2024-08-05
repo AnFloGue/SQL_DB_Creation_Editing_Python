a78141187292334a0199910900e40e6716e837c6
ANflo <75026239+AnFloGue@users.noreply.github.com>
2024-08-05 23:14:45 +0200

    04 Commit: refactoring data_editing.py

--- database.py
+++ database.py
@@ -1,14 +1,15 @@
 import sqlite3
 
+
 class DatabaseConnection:
     def __init__(self, db_path):
         self.db_path = db_path
-
+    
     def connect(self):
         self.conn = sqlite3.connect(self.db_path)
         self.cursor = self.conn.cursor()
         return self.cursor
-
+    
     def disconnect(self, exc_type=None, exc_val=None, exc_tb=None):
         if exc_type is None:
             self.conn.commit()
--- student_operations.py
+++ student_operations.py
@@ -1,9 +1,10 @@
 from database import DatabaseConnection
 
+
 class StudentOperations:
     def __init__(self, db_path):
         self.db_path = db_path
-
+    
     def insert_student(self, student_id, first_name, middle_name, last_name, gpa, email):
         query = """
         INSERT INTO Student (StudentId, FirstName, MiddleName, LastName, Gpa, Email)
@@ -14,7 +15,7 @@ class StudentOperations:
         cursor.execute(query, (student_id, first_name, middle_name, last_name, gpa, email))
         db.disconnect()
         print("Student inserted successfully.")
-
+    
     def delete_student(self, student_id):
         query = "DELETE FROM Student WHERE StudentId = ?"
         db = DatabaseConnection(self.db_path)
@@ -22,7 +23,7 @@ class StudentOperations:
         cursor.execute(query, (student_id,))
         db.disconnect()
         print("Student deleted successfully.")
-
+    
     def edit_student(self, student_id, first_name=None, middle_name=None, last_name=None, gpa=None, email=None):
         query = """
         UPDATE Student
